07b8a79ab119af26589317beceb246dcab24c6d7
derrick-ng <ng.derrick1@hotmail.com>
2026-02-11 12:09:29 -0800

    Create basic test cases for scoring functions

--- backend/listings/tests.py
+++ backend/listings/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 import pandas as pd
 from .etl import clean_listings_data, standardize_location, calculate_quality_score
+from .score_listing import score_price_per_sqft, score_below_market, score_best_value
+from listings.models import Listing
 
 # Create your tests here.
 class ETLTests(TestCase):
@@ -60,3 +62,25 @@ class ETLTests(TestCase):
         cleaned_data = clean_listings_data(raw_data)
         self.assertEqual(len(cleaned_data), 1)
         self.assertEqual(cleaned_data[0]['craigslist_id'], '1')
+
+class ScoringTests(TestCase):
+    def test_score_price_per_sqft(self):
+        listing = Listing(price=1000, sqft=200)
+
+        score = score_price_per_sqft(listing)
+        self.assertEqual(score, 5.0)
+
+    def test_score_best_value(self):
+        listing = Listing(price=3000, sqft=850, location='Nob Hill', bedrooms=2, parking='off_street', laundry_type='in_unit', cats_allowed=True, dogs_allowed=True, data_quality=60)
+        neighborhood_avgs = {'Nob Hill': 3800}
+
+        score = score_best_value(listing, neighborhood_avgs)
+        self.assertGreater(score, 0)
+        self.assertEqual(score, 74)
+    
+    def test_score_below_market(self):
+        listing = Listing(location='Tenderloin', price=1672)
+        neighborhood_avgs = {'Tenderloin': 2000}
+
+        score = score_below_market(listing, neighborhood_avgs)
+        self.assertEqual(score, 16.4)
