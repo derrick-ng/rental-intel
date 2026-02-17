c52a80b69f7ed5fe57dc54a49ff2c76d2f158b01
derrick-ng <ng.derrick1@hotmail.com>
2026-02-16 23:57:30 -0800

    Calculate listing scores during ETL pipeline instead of post-processing

--- backend/listings/management/commands/scrape_listings.py
+++ backend/listings/management/commands/scrape_listings.py
@@ -3,14 +3,18 @@ from listings.models import Listing
 from listings.scraper import scrape_list_urls
 from listings.etl import clean_listings_data
 from listings.geocoding import geocode_address
+from listings.score_listing import score_best_value, score_below_market, score_price_per_sqft
+from listings.analytics import get_neighborhood_stats
 from listings.serializers import ListingSerializer
 import time, requests, os
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         listings_data = scrape_list_urls()
-
         cleaned_data = clean_listings_data(listings_data)
+
+        stats = get_neighborhood_stats()
+        neighborhood_avgs = {stat['location']: stat['avg_price'] for stat in stats}
 
         created_count = 0
         updated_count = 0
@@ -26,6 +30,10 @@ class Command(BaseCommand):
             if duplicate_exists:
                 skipped_count += 1
                 continue
+
+            data['best_value'] = score_best_value(data, neighborhood_avgs)
+            data['below_market'] = score_below_market(data, neighborhood_avgs)
+            data['price_per_sqft'] = score_price_per_sqft(data)
 
             listing, created = Listing.objects.get_or_create(
                 craigslist_id=data['craigslist_id'],
