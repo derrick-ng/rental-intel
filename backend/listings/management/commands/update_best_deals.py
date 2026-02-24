b8b382fc2b788156991df244a2d8156566c72711
derrick-ng <ng.derrick1@hotmail.com>
2026-02-24 02:06:22 -0800

    Complete command to refresh best deals with active listings

--- backend/listings/management/commands/update_best_deals.py
+++ backend/listings/management/commands/update_best_deals.py
@@ -24,17 +24,33 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.clean_below_market_deals()
+        self.clean_best_value_deals()
+        self.clean_best_price_per_sqft_deals()
 
     def clean_below_market_deals(self):
+        print("Below Market deal cleaner start")
+
         inactive_ids = find_inactive_listings(order_by='-below_market')
 
         mark_inactive_in_development(inactive_ids)
         mark_inactive_in_production(inactive_ids)
 
-        print('Below Market deals data refreshed')
+        print('Below Market deals data refreshed\n')
 
     def clean_best_value_deals(self):
-        pass
+        print("Best value deal cleaner start")
+        inactive_ids = find_inactive_listings(order_by='-best_value')
+        
+        mark_inactive_in_development(inactive_ids)
+        mark_inactive_in_production(inactive_ids)
+
+        print('Best value deals data refreshed\n')
 
     def clean_best_price_per_sqft_deals(self):
-        pass
+        print("Best price per sqft cleaner start")
+        inactive_ids = find_inactive_listings(order_by='price_per_sqft')
+
+        mark_inactive_in_development(inactive_ids)
+        mark_inactive_in_production(inactive_ids)
+
+        print('Best price per sqft data refreshed\n')
