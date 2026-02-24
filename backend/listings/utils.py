10de07335e22b97876a40e064559cccb5b48975e
derrick-ng <ng.derrick1@hotmail.com>
2026-02-24 01:30:11 -0800

    Extract listing active validation into reusable utils

--- backend/listings/utils.py
+++ backend/listings/utils.py
@@ -1,6 +1,61 @@
 from listings.models import Listing
 from listings.serializers import ListingSerializer
-import os, requests
+from bs4 import BeautifulSoup
+import os, requests, time, random
+
+def find_inactive_listings(order_by=None):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36"
+    }
+
+    listings = Listing.objects.filter(
+        active=True
+    )
+
+    if order_by:
+        listings = listings.order_by(order_by)
+
+    active_listings = 0
+    inactive_ids = []
+
+    for listing in listings:
+        if active_listings >= 10:
+            break
+
+        time.sleep(random.uniform(3, 5))
+
+        print(f'[{active_listings} active] Checking: {listing.url}')
+
+        try:
+            response = requests.get(listing.url, headers=headers, timeout=10)
+            response.raise_for_status()
+        except Exception as e:
+            print(f'Error fetching inactive listing: {e}')
+            continue
+
+        soup = BeautifulSoup(response.content, 'lxml')
+        removed = soup.find('div', id='has_been_removed')
+
+        if removed:
+            print(f'Listing has been flagged/removed')
+            inactive_ids.append(listing.craigslist_id)
+            continue
+        
+        print('Listing is active')
+        active_listings += 1
+    
+    return inactive_ids
+
+def mark_inactive_in_development(inactive_ids):
+    if not inactive_ids:
+        print('No listings marked as inactive')
+        return
+    
+    updated = Listing.objects.filter(
+        craigslist_id__in=inactive_ids
+    ).update(active=False)
+
+    print(f"DEVELOPMENT: Marked {updated} listings as inactive")
 
 def mark_inactive_in_production(inactive_ids):
     if not inactive_ids:
