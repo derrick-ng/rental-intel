47ca280d93daf3f8d2cca32832d38270a3e8d1c9
derrick-ng <ng.derrick1@hotmail.com>
2026-03-07 20:16:21 -0800

    Create sample gemini ai api function

---
+++ backend/listings/gemini_ai.py
@@ -0,0 +1,13 @@
+from google import genai
+import os
+
+def test_ai():
+    GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+    client = genai.Client(api_key=GEMINI_API_KEY)
+
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview",
+        contents="Explain how AI works in a few words",
+    )
+
+    print(response.text)
