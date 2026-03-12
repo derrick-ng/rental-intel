b2b0fa5117bbea8a6af557bd248ebe32e2e2c199
derrick-ng <ng.derrick1@hotmail.com>
2026-03-12 02:59:56 -0700

    Add try except block to gemini api call

--- backend/listings/gemini_ai.py
+++ backend/listings/gemini_ai.py
@@ -2,12 +2,16 @@ from google import genai
 import os
 
 def test_ai():
-    GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
-    client = genai.Client(api_key=GEMINI_API_KEY)
+    try:
+        GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+        client = genai.Client(api_key=GEMINI_API_KEY)
 
-    response = client.models.generate_content(
-        model="gemini-3-flash-preview",
-        contents="Explain how AI works in a few words",
-    )
+        response = client.models.generate_content(
+            model="gemini-3-flash-preview",
+            contents="Explain how AI works in a few words",
+        )
 
-    print(response.text)
+        return response.text
+    except Exception as e:
+        print(f'Gemini API error: {str(e)}')
+        raise Exception("AI service unavailable")
